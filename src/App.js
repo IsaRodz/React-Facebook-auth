bb23feec4b6c02777ad4b926b2337e3075f927ca
IsaRodz <rodrigueziriarte96@gmail.com>
2020-02-19 12:42:13 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    isLoggedIn: false,
+    userID: null,
+    name: null,
+    email: null,
+    picture: null
+  }
+
+  componentClicked = () => console.log('clicked');
+
+  responseFacebook = response => {
+    console.log(response)
+    this.setState({
+      isLoggedIn: true,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url
+    })
+  }
+
+  render() {
+
+    let fbContent
+
+    if (this.state.isLoggedIn) {
+
+      fbContent = (
+        <div style={{
+          background: "#f7f7f7",
+          padding: "20px",
+          borderRadius: "3px",
+          maxWidth: "400px",
+          margin: "20px auto"
+        }}>
+          <img src={this.state.picture} alt={this.state.name} />
+          <h3>Welcome, {this.state.name}</h3>
+          <p>{this.state.email}</p>
+        </div>
+      )
+
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId="2215691505186605"
+          autoLoad={true}
+          fields="name, email, picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      )
+    }
+
+    return (
+      <div className="app">
+        <h1>React-Facebook Login</h1>
+        {fbContent}
+      </div>
+    );
+  }
 }
 
 export default App;
